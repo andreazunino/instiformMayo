183c478d33552ed776b40f07a12f550d54ca0f6e
andreazunino <andreab.zunino@gmail.com>
2025-05-28 16:34:58 -0300

    correcciones en menu administrados

--- app/controllers/menuAdministrador.php
+++ app/controllers/menuAdministrador.php
@@ -1,11 +1,12 @@
 <?php
-require_once __DIR__ . '/../lib/Smarty/libs/Autoloader.php';
-Smarty_Autoloader::register();
-
 require_once __DIR__ . '/../lib/Smarty/libs/Smarty.class.php';
 
-$smarty = new Smarty();
+$smarty = new Smarty\Smarty;  
 $smarty->setTemplateDir(__DIR__ . '/../views/');
 $smarty->setCompileDir(__DIR__ . '/../templates_c/');
 
 $smarty->display('menuAdministrador.tpl');
+
+
+
+            
--- app/templates_c/3b91ce0c73169aafbadcacace9257b8ad8ece73b_0.file_menuAdministrador.tpl.php
+++ app/templates_c/3b91ce0c73169aafbadcacace9257b8ad8ece73b_0.file_menuAdministrador.tpl.php
@@ -1,17 +1,49 @@
-<!DOCTYPE html>
+<?php
+/* Smarty version 5.4.0, created on 2025-05-28 21:31:19
+  from 'file:menuAdministrador.tpl' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.0',
+  'unifunc' => 'content_683764874b7036_57831708',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '3b91ce0c73169aafbadcacace9257b8ad8ece73b' => 
+    array (
+      0 => 'menuAdministrador.tpl',
+      1 => 1748460674,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:head.tpl' => 1,
+  ),
+))) {
+function content_683764874b7036_57831708 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\InstiformMayo\\app\\views';
+?><!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    {include 'head.tpl'}
+    <?php $_smarty_tpl->renderSubTemplate('file:head.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+?>
 
 <style>
+   
+
     .logo-small {
         max-width: 80px;
         margin-top: 10px;
     }
-</style>
-</head>
 
+</style>
+
+       
+
+</head>
 <body>
+
 
 <!-- Botón de cerrar sesión -->
 <button class="btn btn-logout" onclick="window.location.href='../../index.php'">Cerrar sesión</button>
@@ -82,9 +114,17 @@
 </div>
 
 <!-- Scripts Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.5.1.slim.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
 
 </body>
 </html>
+<?php }
+}
